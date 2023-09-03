e34bf9923724e90acad8b4906170d60ae2cc92b4
Evan Joseph <evandromariano49@gmail.com>
2023-09-03 09:41:14 -0300

    feat: ng-class

--- src/app/comp-atributos/comp-atributos.component.ts
+++ src/app/comp-atributos/comp-atributos.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-comp-atributos',
   templateUrl: './comp-atributos.component.html',
-  styleUrls: ['./comp-atributos.component.css']
+  styleUrls: ['./comp-atributos.component.css'],
 })
 export class CompAtributosComponent {
-
+  mudaCor() {
+    if (this.estilo == 'enable') {
+      this.estilo = 'disable';
+    } else {
+      this.estilo = 'enable';
+    }
+  }
+  estilo: string = 'enable';
 }
