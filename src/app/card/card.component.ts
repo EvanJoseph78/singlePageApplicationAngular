1f53b2b2a6e5c64b1c065527c9fcc7d5031a952b
Evan Joseph <evandromariano49@gmail.com>
2023-09-03 09:29:10 -0300

    feat: ngSwitch

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,16 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  menuType: string = "superuser";
+  remover(index: number) {
+    this.produtos.splice(index, 1);
+  }
+  adicionar() {
+    this.produtos.push('evandro');
+  }
   produtos: string[] = [];
 
   constructor() {
-    this.produtos= [
-      "mouse",
-      "teclado",
-      "cabo",
-      "font",
-      "galaxy book 3-360"
-    ]
+    this.produtos = ['mouse', 'teclado', 'cabo', 'font', 'galaxy book 3-360'];
   }
 
   ngOnInit(): void {}
